16c7f2479522782f8055dc2ed259a9a3a8d969bc
VittorioDL <victor03.VDL@gmail.com>
2024-04-22 17:37:49 +0200

    adc in cpp

--- rpi_pico_xbee/src/main.cpp
+++ rpi_pico_xbee/src/main.cpp
@@ -16,26 +16,26 @@
 #define XBEE_UART_NODE DT_CHOSEN(cansat_xbee)
 
 //BME-------------------------------------------------------------
-static const struct device *get_bme280_device(void)
-{
-	const struct device* dev = DEVICE_DT_GET_ANY(bosch_bme280);
+// static const struct device *get_bme280_device(void)
+// {
+// 	const struct device* dev = DEVICE_DT_GET_ANY(bosch_bme280);
 
-	if (dev == NULL) {
-		/* No such node, or the node does not have status "okay". */
-		printk("\nError: no device found.\n");
-		return NULL;
-	}
+// 	if (dev == NULL) {
+// 		/* No such node, or the node does not have status "okay". */
+// 		printk("\nError: no device found.\n");
+// 		return NULL;
+// 	}
 
-	if (!device_is_ready(dev)) {
-		printk("\nError: Device \"%s\" is not ready; "
-		       "check the driver initialization logs for errors.\n",
-		       dev->name);
-		return NULL;
-	}
+// 	if (!device_is_ready(dev)) {
+// 		printk("\nError: Device \"%s\" is not ready; "
+// 		       "check the driver initialization logs for errors.\n",
+// 		       dev->name);
+// 		return NULL;
+// 	}
 
-	printk("Found device \"%s\", getting sensor data\n", dev->name);
-	return dev;
-}
+// 	printk("Found device \"%s\", getting sensor data\n", dev->name);
+// 	return dev;
+// }
 
 int main(void)
 {
@@ -56,16 +56,16 @@ int main(void)
         gpio_pin_toggle_dt(&led_dev);
 
         //BME--------------------------------------------------------------
-        const struct device *bme_dev = get_bme280_device();
-        if (bme_dev == NULL) {
-            return 0;
-        }
-        struct sensor_value temp, press, humidity;
+        // const struct device *bme_dev = get_bme280_device();
+        // if (bme_dev == NULL) {
+        //     return 0;
+        // }
+        // struct sensor_value temp, press, humidity;
 
-		sensor_sample_fetch(bme_dev);
-		sensor_channel_get(bme_dev, SENSOR_CHAN_AMBIENT_TEMP, &temp);
-		sensor_channel_get(bme_dev, SENSOR_CHAN_PRESS, &press);
-		sensor_channel_get(bme_dev, SENSOR_CHAN_HUMIDITY, &humidity);
+		// sensor_sample_fetch(bme_dev);
+		// sensor_channel_get(bme_dev, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+		// sensor_channel_get(bme_dev, SENSOR_CHAN_PRESS, &press);
+		// sensor_channel_get(bme_dev, SENSOR_CHAN_HUMIDITY, &humidity);
 
 		// printk("temp: %d.%06d; press: %d.%06d; humidity: %d.%06d\n",
 		//       temp.val1, temp.val2, press.val1, press.val2,
@@ -75,7 +75,7 @@ int main(void)
         // tx_req.set_payload("temp: "+std::to_string(temp.val1)+"."+std::to_string(temp.val2)+
         //                    " ,press: "+std::to_string(press.val1)+"."+std::to_string(press.val2)+
         //                    " ,humidity: "+std::to_string(humidity.val1)+"."+std::to_string(humidity.val2));
-		tx_req.set_payload("los pollos hermanos");
+		tx_req.set_payload("1066,13:14:02.22,1000,F,LAUNCH_WAIT,800.1,100.1,N,N,37.1,5.0,121.1,13:14:02.22,700.1,-37.4003,-37.4003");
         tx_req.calculate_length();
         tx_req.calculate_checksum();
         xbee.send_request(tx_req);
